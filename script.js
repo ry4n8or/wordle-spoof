bc904a18f6c6e7f64f1d0a9b9b4469ae0e5c2ebc
RyanDeLong <rdelong111@gmail.com>
2022-02-11 13:36:05 -0600

    Working game minus checking if entered word is in dictionary

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 	createLetterSpots(document.getElementById('letter-ctr'));
 
+	let row = 0;
+	let column = 0;
+	const theWord = 'ULCER';
+
 	const buttons = document.querySelectorAll('button');
 	for (let button = 0; button < buttons.length; button++) {
 		buttons[button].addEventListener('click', () => {
-			console.log(buttons[button].value)
+			if (buttons[button].value === 'ENTER' && column === 5) {
+				checkWord(theWord, row);
+				row++;
+				column = 0;
+			}
+			else if (buttons[button].value === 'DELETE' && column > 0) {
+				column--;
+				gamedelete(row, column);
+			}
+			else if ((buttons[button].value !== 'DELETE' && buttons[button].value !== 'ENTER') && row <= 5 && column <= 4) {
+				game(row, column, buttons[button].value);
+				column++;
+			}
 		});
 	}
 });
@@ -25,4 +41,75 @@ function createSpot(row, spot) {
 	Ltr.textContent = '#';
 
 	currentRow.appendChild(Ltr);
+}
+
+function game(row, column, ltr) {
+	const piece = document.getElementById(`r${row}c${column}`);
+	piece.textContent = ltr;
+	piece.style.color = '#ffffff';
+}
+
+function gamedelete(row, column) {
+	const piece = document.getElementById(`r${row}c${column}`);
+	piece.textContent = '#';
+	piece.style.color = '#000000';
+}
+
+function checkWord(theWord, row) {
+	let correct = 0;
+	const words = document.querySelectorAll(`#row-${row} div`);
+	const word = `${words[0].textContent}${words[1].textContent}${words[2].textContent}${words[3].textContent}${words[4].textContent}`;
+	console.log(word)
+	const wordOb = {};
+	for (let i = 0; i < 5; i++) {
+		if (theWord[i] in wordOb) {
+			wordOb[theWord[i]]++;
+		}
+		else {
+			wordOb[theWord[i]] = 1;
+		}
+	}
+
+	// green check
+	for (let j = 0; j < 5; j++) {
+		if (word[j] === theWord[j]) {
+			words[j].style.background = 'green';
+			words[j].style.color = 'black';
+			correct++;
+			document.getElementById(word[j]).style.backgroundColor = 'green';
+			if (wordOb[word[j]] === 1) {
+				delete wordOb[word[j]];
+			}
+			else {
+				wordOb[word[j]]--;
+			}
+		}
+	}
+
+	if (correct === 5) {
+		alert('YOU WIN');
+	}
+
+	// yellow or black check
+	for (let k = 0; k < 5; k++) {
+		if (word[k] in wordOb) {
+			words[k].style.background = 'yellow';
+			words[k].style.color = 'black';
+			if (document.getElementById(word[k]).style.backgroundColor !== 'green') {
+				document.getElementById(word[k]).style.backgroundColor = 'yellow';
+			}
+			if (wordOb[word[k]] === 1) {
+				delete wordOb[word[k]];
+			}
+			else {
+				wordOb[word[k]]--;
+			}
+		}
+		else {
+			if (document.getElementById(word[k]).style.backgroundColor !== 'green' && document.getElementById(word[k]).style.backgroundColor !== 'yellow') {
+				document.getElementById(word[k]).style.backgroundColor = 'black';
+				document.getElementById(word[k]).style.color = 'white';
+			}
+		}
+	}
 }
