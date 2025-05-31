ed754ff0f234dfe80587d602d9eec9c004aa2331
Ryan DeLong <ry4n8or-accounts@pm.me>
2025-05-30 23:15:35 -0400

    finding out error code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	let row = 0;
 	let column = 0;
-	const theWord = 'CLONE'; // Got to refigure out how to access my list
+	const theWord = 'CLONE'; // error code when git push
 
 	const buttons = document.querySelectorAll('button');
 	for (let button = 0; button < buttons.length; button++) {
